29186922c2145502f9b2e80010cfe54a6305dfcf
Stella <syampolsky50@stuy.edu>
2024-11-26 14:50:46 -0500

    end

--- main.c
+++ main.c
@@ -4,6 +4,26 @@
 #include <sys/wait.h>
 
 int main(){
-  child = fork();
-  return 0;
+  printf("%d about to create 2 child processes", getpid());
+  pid_t p;
+  pid_t b;
+  p = fork();
+  if(p<0){
+    perror("fork fail");
+    exit(1)
+  }
+  else if(p > 0){
+    b = fork();
+    wait(WIFEXITED(int* status));
+    printf("Main Process %d is done, child ")
+  }
+  else if(p == ){
+    printf("PID: %d\n", getpid());
+    srand(getpid());
+    int x = rand() % 5 + 1;
+    printf("random number: %d\n", x);
+    sleep(x);
+    print("%d finished after %d seconds", getpid(), x);
+    exit();
+ }
 }
