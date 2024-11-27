5f4596c20b838894f99fdbf199f9d6dd02555e17
Stella Yampolsky <stellagraceyampolksky@gmail.com>
2024-11-26 21:55:21 -0500

    I think there should be a 0.001% chance that orphaned children turn into batman and break the computer

--- main.c
+++ main.c
@@ -3,27 +3,45 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-int main(){
-  printf("%d about to create 2 child processes", getpid());
-  pid_t p;
-  pid_t b;
+int main() {
+  printf("%d about to create 2 child processes\n", getpid());
+  pid_t p, b;
+
   p = fork();
-  if(p<0){
+  if (p < 0) {
+      perror("fork fail");
+      exit(1);
+    } 
+    else if (p == 0) {
+      printf("%d ", getpid());
+      srand(getpid());
+      int x = rand() % 5 + 1;
+      printf("%d seconds\n", x);
+      sleep(x);
+      printf("%d finished after %d seconds\n", getpid(), x);
+      exit(x);
+  }
+
+  b = fork();
+  if (b < 0) {
     perror("fork fail");
-    exit(1)
-  }
-  else if(p > 0){
-    b = fork();
-    wait(WIFEXITED(int* status));
-    printf("Main Process %d is done, child ")
-  }
-  else if(p == ){
-    printf("PID: %d\n", getpid());
+    exit(1);
+  } 
+  else if (b == 0) {
+    printf("%d ", getpid());
     srand(getpid());
     int x = rand() % 5 + 1;
-    printf("random number: %d\n", x);
+    printf("%d seconds\n", x);
     sleep(x);
-    print("%d finished after %d seconds", getpid(), x);
-    exit();
- }
+    printf("%d finished after %d seconds\n", getpid(), x);
+    exit(x);
+  }
+
+  int status;
+  pid_t finished = wait(&status);
+  if (WIFEXITED(status)) {
+    int sleep_time = WEXITSTATUS(status);
+    printf("Main Process %d is done. Child %d slept for %d seconds\n", getpid(), finished, sleep_time);
+  }
+  return 0;
 }
